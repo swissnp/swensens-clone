7d059cb4ae7a41615c65925d702ed2da46caf55b
swiss0404 <swiss0404@gmail.com>
2023-03-26 19:52:05 +0700

    add registration success toast

--- src/components/SexSelector.tsx
+++ src/components/SexSelector.tsx
@@ -1,28 +1,31 @@
 import { useState } from "react";
+import type { UseFormTrigger } from "react-hook-form";
 
-const SexSelector = (
-  {
-    set
-  }:{
-    set: (name: 'sex', value: "male" | "female" | "unidentify") => void
-  }
-) => {
+const SexSelector = ({
+  set,
+  trigger,
+}: {
+  set: (name: "sex", value: "male" | "female" | "unidentify") => void;
+  trigger: UseFormTrigger<any>;
+}) => {
   const [sex, setSex] = useState<"male" | "female" | "unidentify" | "">("");
   return (
     <div>
       <label className="relative mr-2 mb-2 ml-px cursor-default text-neutral-700">
         เพศ (ไม่ระบุได้)
       </label>
-      <div className="py-1 mb-2">
+      <div className="mb-2 py-1">
         <button
-          value={'male'}
+          value={"male"}
           className={`btn-primary no-animation btn mr-2 rounded-3xl ${
             sex === "male" ? "btn-active" : "btn-outline"
           }`}
-          onClick={(e) => {
+          onClick={async (e) => {
             e.preventDefault();
             setSex("male");
-            set('sex',"male")
+            set("sex", "male");
+            console.log("e");
+            await trigger("sex");
           }}
         >
           ชาย
@@ -31,10 +34,11 @@ const SexSelector = (
           className={`btn-primary no-animation btn mr-2 rounded-3xl ${
             sex === "female" ? "btn-active" : "btn-outline"
           }`}
-          onClick={(e) => {
+          onClick={async (e) => {
             e.preventDefault();
             setSex("female");
-            set('sex',"female")
+            set("sex", "female");
+            await trigger("sex");
           }}
         >
           หญิง
@@ -43,10 +47,11 @@ const SexSelector = (
           className={`btn-primary no-animation btn mr-2 rounded-3xl ${
             sex === "unidentify" ? "btn-active" : "btn-outline"
           }`}
-          onClick={(e) => {
+          onClick={async (e) => {
             e.preventDefault();
             setSex("unidentify");
-            set('sex',"unidentify")
+            set("sex", "unidentify");
+            await trigger("sex");
           }}
         >
           ไม่ระบุ
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -5,14 +5,16 @@ import Header from "~/components/Header";
 import SignInForm from "~/components/SignInForm";
 import { type ILogin } from "~/common/validation/auth";
 import { signIn } from "next-auth/react";
-import  { type SignInResponse } from "next-auth/react";
-
+import { type SignInResponse } from "next-auth/react";
+import RegisSuccess from "~/components/RegisSuccess";
 const Home: NextPage = () => {
-  const onSubmit = async (data: ILogin): Promise<SignInResponse | undefined> => {
-    const response = await signIn("credentials", { ...data, redirect : false })
-    return response
-  }
-  
+  const onSubmit = async (
+    data: ILogin
+  ): Promise<SignInResponse | undefined> => {
+    const response = await signIn("credentials", { ...data, redirect: false });
+    return response;
+  };
+
   return (
     <>
       <Head>
@@ -21,12 +23,13 @@ const Home: NextPage = () => {
         <link rel="icon" href="/fav-sw.jpg" />
       </Head>
       <main className="flex min-h-screen flex-col items-center font-kanit tabular-nums">
+        <RegisSuccess />
         <Header />
         <section className="mt-16 flex flex-auto flex-col bg-none py-10 font-kanit font-light leading-5 text-neutral-500">
           <section className="flex flex-auto flex-col">
             <main className="block flex-auto font-kanit font-light leading-5 text-neutral-500">
               <div className="mx-autoleading-5 px-6 text-neutral-500">
-                <div className=" mx-auto flex flex-col rounded-[10px] w-[480px] bg-white leading-5 text-neutral-500">
+                <div className=" mx-auto flex w-[480px] flex-col rounded-[10px] bg-white leading-5 text-neutral-500">
                   <div className="relative flex h-60 grow flex-col items-start justify-center bg-zinc-300 bg-contain object-cover py-8 px-10 font-kanit font-light leading-5 text-white">
                     <div className="z-10 bg-contain text-white ">
                       <h2 className="text-3xl">ยินดีต้อนรับ</h2>
@@ -35,7 +38,7 @@ const Home: NextPage = () => {
                     <Image src={"/card-head.png"} fill alt="card-head" />
                   </div>
                   <div className="p-10 font-kanit font-light leading-5 text-neutral-500">
-                    <SignInForm onSubmit={onSubmit}/>
+                    <SignInForm onSubmit={onSubmit} />
                   </div>
                 </div>
               </div>
